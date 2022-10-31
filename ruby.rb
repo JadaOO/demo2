c97104a3f35eb25410ae33feacf1844f380c26ca
JadaOO <98721834+JadaOO@users.noreply.github.com>
2022-10-31 13:07:31 -0700

    add ruby

---
+++ ruby.rb
@@ -0,0 +1,11 @@
+happy=55
+case happy
+     when happy < 50
+        puts "you are not happy"
+
+     when happy > 50 and  happy < 100
+        puts "you are calm today"
+
+     else
+        puts "you are very happy"
+end
